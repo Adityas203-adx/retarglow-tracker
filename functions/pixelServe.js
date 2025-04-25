865663254e6de7f5416ca0dd5bd7e269b53a3ae7
Adityas203-adx <aditya@adsynergix.com>
2025-04-26 00:54:13 +0530

    Update and rename loadscript.js to pixelServe.js

---
+++ functions/pixelServe.js
@@ -0,0 +1,11 @@
+exports.handler = async (event) => {
+  const id = event.queryStringParameters?.id || 'unknown';
+
+  const script = `(function(){try{var a=document.cookie.match(/retarglow_id=([^;]+)/)?.[1];fetch('https://retarglow.com/pixel/t.gif',{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({custom_id:'${id}'}),credentials:'include'});}catch(e){}})();`;
+
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'application/javascript' },
+    body: script,
+  };
+};
