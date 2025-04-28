3b73b16dc241b1d45a5c17220846c8e42fe9ee4b
Adityas203-adx <aditya@adsynergix.com>
2025-04-29 02:53:50 +0530

    Update track.js

--- functions/track.js
+++ functions/track.js
@@ -6,7 +6,7 @@ const UAParser = require('ua-parser-js');
 
 // Supabase config
 const SUPABASE_URL = 'https://nandqoilqwsepborxkrz.supabase.co';
-const SUPABASE_KEY = 'your_supabase_key_here'; // ← safer with env vars
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5hbmRxb2lscXdzZXBib3J4a3J6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzNTkwODAsImV4cCI6MjA2MDkzNTA4MH0.FU7khFN_ESgFTFETWcyTytqcaCQFQzDB6LB5CzVQiOg'; // ← safer with env vars
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // GA4 config
